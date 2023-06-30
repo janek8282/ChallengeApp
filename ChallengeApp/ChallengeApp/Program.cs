27d650d8f43b0d52ebe3bca469e15c1d8a5c976c
Grzegorz Gołubiński <janek8282@gmail.com>
2023-06-30 09:10:52 +0200

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var genderWoman = true;
+var age = 31;
+
+if (genderWoman == true)
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (age == 33 && name == "Ewa")
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else
+    {
+        Console.WriteLine("Kobieta w wieku 30+ lat");
+    }
+}
+else
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni Męszczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Pełnoletni Męszczyzna");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
