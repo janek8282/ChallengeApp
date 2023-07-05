3167fa982fcb9b415a0ea265570301c3a42058bf
Grzegorz Gołubiński <janek8282@gmail.com>
2023-07-05 20:37:13 +0200

    Zadanie dzień 6

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,31 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+        private List<int> scores = new List<int>();
+
+        public Employee(string name, string surName, int age)
+        {
+            this.Name = name;
+            this.SurName = surName;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+        public string SurName { get; private set; }
+        public int Age { get; private set; }
+
+        public int SumOfScores
+        {
+            get
+            {
+                return this.scores.Sum();
+            }
+        }
+        public void AddScore(int score)
+        {
+            this.scores.Add(score);
+        }
+    }
+}
+
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,19 +1,37 @@
-﻿using ChallengeApp;//korzystamy z przestrzeni naz i mamy dostęp do klasy USer, która jest tam zawarta
-//każdą klasę należy umieszczać w osobnym pliku klasy
-User user1 = new User("Ala", "11");
-User user2 = new User("Ela", "22");
-User user3 = new User("Ola", "33");
-User user4 = new User("Wiola", "44");
+﻿using ChallengeApp;
 
+var employee1 = new Employee("Ala", "Kloc", 28);
+var employee2 = new Employee("Ela", "Pałac", 35);
+var employee3 = new Employee("Ola", "Miła", 41);
 
-var loginName1 = user1.Login;
+employee1.AddScore(4);  employee2.AddScore(9);  employee3.AddScore(5);
+employee1.AddScore(7);  employee2.AddScore(6);  employee3.AddScore(5);
+employee1.AddScore(3);  employee2.AddScore(1);  employee3.AddScore(9);
+employee1.AddScore(9);  employee2.AddScore(3);  employee3.AddScore(3);
+employee1.AddScore(7);  employee2.AddScore(8);  employee3.AddScore(4);
 
-user1.AddScore(5);
-user1.AddScore(7);
-var result = user1.Result;
-Console.WriteLine(loginName1 + " \nSuma punktów to: " + result);
+var employee1SumOfScores = employee1.SumOfScores;
+var employee2SumOfScores = employee2.SumOfScores;
+var employee3SumOfScores = employee3.SumOfScores;
 
+int maxValueOfScores = employee1SumOfScores;
+Employee employee;
 
-
-
-
+if (maxValueOfScores < employee2SumOfScores)
+{
+    maxValueOfScores = employee2SumOfScores;
+    employee = employee2;
+}
+else if (maxValueOfScores < employee3SumOfScores)
+{
+    maxValueOfScores = employee3SumOfScores;
+    employee = employee3;
+}
+else
+{
+    employee = employee1;
+}
+Console.WriteLine("Njawiększą sumę punktów --> "
+    + maxValueOfScores + " uzyskał pracownik:\n" 
+    + employee.Name + " " + employee.SurName + "\n" 
+    + employee.Age + " lat");
