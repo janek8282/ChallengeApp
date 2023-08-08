cff140abd960accbb7a434569fecdf5fe151c6ce
janek <janek8282@gmail.com>
2023-08-08 22:56:40 +0200

    Dzień 16: Praca z klasami bazowymi i interface’amiMetoda abstrakcyjna

---
+++ ChallengeApp/ChallengeApp/EmployeeBase.cs
@@ -0,0 +1,21 @@
+﻿namespace ChallengeApp
+{
+    public abstract class EmployeeBase: IEmployee
+    {
+        public EmployeeBase(string name, string surname)
+        {
+            this.Name = name;
+            this.SurName = surname;
+        }
+        public string Name { get; private set; }
+        public string SurName { get; private set; }
+        //dodając "abstract" do metody nie trzeba jej definiować w tej klasie,
+        //można to zrobić w kolejnej klasie
+        public abstract void AddGrade(float grade);
+        public abstract void AddGrade(long grade);   
+        public abstract void AddGrade(double grade);
+        public abstract void AddGrade(char grade);
+        public abstract void AddGrade(string grade);        
+        public abstract Statistics GetStatistics();
+    }
+}
---
+++ ChallengeApp/ChallengeApp/EmployeeInFile.cs
@@ -0,0 +1,41 @@
+﻿namespace ChallengeApp
+{
+    public abstract class EmployeeInFile : EmployeeBase
+    {
+        public EmployeeInFile(string name, string surname) 
+            : base(name, surname)
+        {
+
+        }
+
+        public override void AddGrade(float grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AddGrade(long grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AddGrade(double grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AddGrade(char grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AddGrade(string grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Statistics GetStatistics()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
